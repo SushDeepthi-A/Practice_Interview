8041b9fc1a18bcc212a7283fb8444ce74fb315d5
sushDeepthi-A <a.sushma.deepthi@gmail.com>
2021-04-10 11:17:11 +0530

    Modified the file

--- Swap.py
+++ Swap.py
@@ -1,7 +1,9 @@
-$Swapping the first element to last element and Last element to First Element
-List = [10, 20, 30, 40, 50]
+#Swapping the first element to last element and Last element to First Element
+
 def swap(l1):
-  new[0]=l1[-1]
-  new[-1]=l1[0]
-  print(new)
-swap(list)
+  l1[0],l1[-1] = l1[-1], l1[0]
+  return l1
+List = [10, 20, 30, 40, 50]
+print(swap(List))
+
+
